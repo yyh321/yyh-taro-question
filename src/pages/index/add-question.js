b0746fb0db01e372ab7453de2902f89c3822d42f
yyh123 <yunhaoyu@gmail.com>
2019-02-28 23:05:33 +0800

    数据持久化

--- src/pages/index/add-question.js
+++ src/pages/index/add-question.js
@@ -8,23 +8,68 @@ export default class AddQuestion extends Component {
 
   constructor(props) {
     super(props)
+    this.state={
+      title: '',
+      des: ''
+    }
 
     this.handleCancelClick = this.handleCancelClick.bind(this)
+    this.handleConfirmClick = this.handleConfirmClick.bind(this)
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleQuestionContentChange = this.handleQuestionContentChange.bind(this)
   }
 
+  // 点击取消关闭窗口
   handleCancelClick() {
     const {onCancelQuestion} = this.props
     onCancelQuestion()
   }
+
+  // 点击确定
+  handleConfirmClick() {
+    const title = this.state.title.trim()
+    const des = this.state.des.trim()
+    if(!title || !des) {
+      Taro.showToast({
+        title: '请填写标题或问题',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    const {onGetQuestionData} = this.props
+    onGetQuestionData({id:parseInt(Math.random()*10000),title:this.state.title, des: this.state.des});
+    Taro.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000,
+      success:()=>{
+        this.handleCancelClick()
+      }
+    })
+  }
+
+  // 标题输入
+  handleTitleChange(e) {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  // 问题内容输入
+  handleQuestionContentChange(e) {
+    this.setState({des: e.target.value})
+  }
+
   render() {
     return (
       <Dialog>
         <View className='add-question'>
           <View className='question-container'>
-            <Input className='question,question-title' placeholder='请输入问题标题' />
-            <Textarea className='question,question-desc' placeholder='请输入问题描述' />
+            <Input onInput={this.handleTitleChange} className='question,question-title' placeholder='请输入问题标题' />
+            <Textarea onInput={this.handleQuestionContentChange} className='question,question-desc' placeholder='请输入问题描述' />
             <View className='btn-group'>
-              <Button className='question-btn question-ok'>确定</Button>
+              <Button onClick={this.handleConfirmClick} className='question-btn question-ok'>确定</Button>
               <Button onClick={this.handleCancelClick} className='question-btn'>取消</Button>
             </View>
           </View>
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,13 +4,30 @@ import { View,Button } from '@tarojs/components'
 import AddQuestion from './add-question'
 import './index.less'
 
+function getStore(key) {
+  let str = Taro.getStorageSync(key);
+  if(!str) {
+    return []
+  }
+  return JSON.parse(str);
+}
+
+function setStore(key, obj) {
+  let str = obj
+  if(typeof str =='object') {
+    str = JSON.stringify(obj)
+  }
+  Taro.setStorageSync(key,str)
+}
+
 export default class Index extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {isShowModel: false}
-    this.addQuestion = this.addQuestion.bind(this);
-    this.cancelQuestion = this.cancelQuestion.bind(this);
+    this.state = {isShowModel: false,list:getStore('questions')}
+    this.addQuestion = this.addQuestion.bind(this)
+    this.cancelQuestion = this.cancelQuestion.bind(this)
+    this.getQuestionData = this.getQuestionData.bind(this)
   }
 
   config = {
@@ -26,12 +43,32 @@ export default class Index extends Component {
     this.setState({isShowModel: false})
   }
 
+  // 获取问题内容
+  getQuestionData(question){
+    this.setState({
+      list:[...this.state.list,question]
+    },() => {
+      setStore('questions',this.state.list)
+    })
+  }
+
   render () {
     return (
       <View className='index'>
         <View className='header-title'>问答示例</View>
-        {this.state.isShowModel ? <AddQuestion onCancelQuestion={this.cancelQuestion} /> : null}
-
+        <View className='container'>
+          {
+            this.state.list.map(item => {
+              return (
+                <View className='question-item' key={item.id}>
+                  <View className='question-item-title'>{item.title}</View>
+                  <View className='question-item-des'>{item.des}</View>
+                </View>
+              )
+            })
+          }
+        </View>
+        {this.state.isShowModel ? <AddQuestion onGetQuestionData={this.getQuestionData} onCancelQuestion={this.cancelQuestion} /> : null}
         <Button onClick={this.addQuestion} className='btn-question'>提问</Button>
       </View>
     )
